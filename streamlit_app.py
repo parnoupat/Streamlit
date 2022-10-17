c2bc598e88793cf28a3b03cc31341b531c3857e6
parnoupat <parnoupat.n@icloud.com>
2022-10-17 21:26:27 +0700

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,106 @@
-# streamlit_app.py
+# Streamlit-Google Sheet
+## Modules
+import streamlit as st 
+from pandas import DataFrame
 
-import streamlit as st
-from gsheetsdb import connect
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
 
-# Create a connection object.
-conn = connect()
+# Application Related Module
+import pubchempy as pcp
+from pysmiles import read_smiles
+# 
+import networkx as nx
+import matplotlib.pyplot as plt
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+from datetime import datetime
 
-sheet_url = st.secrets["gsheets"]["Sheet1"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# Disable certificate verification (Not necessary always)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "Database"
+spread = Spread(spreadsheetname,client = client)
+
+# Check the connection
+st.write(spread.url)
+
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
+
+# Functions 
+@st.cache()
+# Get our worksheet names
+def worksheet_names():
+    sheet_names = []   
+    for sheet in worksheet_list:
+        sheet_names.append(sheet.title)  
+    return sheet_names
+
+# Get the sheet as dataframe
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = DataFrame(worksheet.get_all_records())
+    return df
+
+# Update to Sheet
+def update_the_spreadsheet(spreadsheetname,dataframe):
+    col = ['Compound CID','Time_stamp']
+    spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
+    st.sidebar.info('Updated to GoogleSheet')
+
+
+st.header('Streamlit Chemical Inventory')
+
+# Check whether the sheets exists
+what_sheets = worksheet_names()
+#st.sidebar.write(what_sheets)
+ws_choice = st.sidebar.radio('Available worksheets',what_sheets)
+
+# Load data from worksheets
+df = load_the_spreadsheet(ws_choice)
+# Show the availibility as selection
+select_CID = st.sidebar.selectbox('CID',list(df['Compound CID']))
+
+# Now we can use the pubchempy module to dump information
+comp = pcp.Compound.from_cid(select_CID)
+comp_dict = comp.to_dict() # Converting to a dictinoary
+# What Information look for ?
+options = ['molecular_weight' ,'molecular_formula',
+           'charge','atoms','elements','bonds']
+show_me = st.radio('What you want to see?',options)
+
+st.info(comp_dict[show_me])
+name = comp_dict['iupac_name']
+st.markdown(name)
+plot = st.checkbox('Canonical Smiles Plot')
+
+if plot:
+    sm = comp_dict['canonical_smiles']
+    mol = read_smiles(comp_dict['canonical_smiles']) 
+    elements = nx.get_node_attributes(mol, name = "element")
+    nx.draw(mol, with_labels=True, labels = elements, pos=nx.spring_layout(mol))
+    fig , ax = plt.subplots()
+    nx.draw(mol, with_labels=True, labels = elements, pos = nx.spring_layout(mol))
+    st.pyplot(fig)
+
+add = st.sidebar.checkbox('Add CID')
+if add :  
+    cid_entry = st.sidebar.text_input('New CID')
+    confirm_input = st.sidebar.button('Confirm')
+    
+    if confirm_input:
+        now = datetime.now()
+        opt = {'Compound CID': [cid_entry],
+              'Time_stamp' :  [now]} 
+        opt_df = DataFrame(opt)
+        df = load_the_spreadsheet('Pending CID')
+        new_df = df.append(opt_df,ignore_index=True)
+        update_the_spreadsheet('Pending CID',new_df)
