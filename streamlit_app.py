c036ab9b8c25b0ee5a801f5b593da7ddd174d140
parnoupat <81098068+parnoupat@users.noreply.github.com>
2022-10-17 18:12:45 +0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
-    ],
+    ]
 )
 conn = connect(credentials=credentials)
 
