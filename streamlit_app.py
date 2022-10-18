1d293535394c4b670a91e5a27db3dc211c38974f
parnoupat <parnoupat.n@icloud.com>
2022-10-18 22:27:10 +0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,19 +92,15 @@ return_value = st_javascript("""
 
   function runApp() {
     liff.getProfile().then(profile => {
-      document.getElementById("pictureUrl").src = profile.pictureUrl;
-      document.getElementById("userId").innerHTML = '<b>UserId:</b> ' + profile.userId;
-      document.getElementById("displayName").innerHTML = '<b>DisplayName:</b> ' + profile.displayName;
-      document.getElementById("statusMessage").innerHTML = '<b>StatusMessage:</b> ' + profile.statusMessage;
-      document.getElementById("getDecodedIDToken").innerHTML = '<b>Email:</b> ' + liff.getDecodedIDToken().email;
+      const  displayname = profile.displayName;
     }).catch(err => console.error(err));
-    return profile.displayName
+    return displayname
   }
   liff.init({ liffId: "1657566121-pOJyJlDk" }, () => {
     if (liff.isLoggedIn()) {
       runApp()
     } else {
-      liff.login();
+      runApp();
     }
   }, err => console.error(err.code, error.message));
 """)
